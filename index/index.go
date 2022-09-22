42ff63b70b4f50aa1eed85fbcd6e234810df7342
whosonfirst <whosonfirst@localhost>
2022-09-22 15:47:23 -0700

    Update to enable creating tables with URI-based syntax

--- index/index.go
+++ index/index.go
@@ -16,13 +16,13 @@ type IndexTablesOptions struct {
 	Database sql.Database
 	Tables   []sql.Table
 	Logger   *log.Logger
-	Timings bool
+	Timings  bool
 }
 
 func IndexTables(ctx context.Context, opts *IndexTablesOptions, iterator_uri string, to_iterate ...string) error {
 
 	var monitor timings.Monitor
-	
+
 	iter_cb := func(ctx context.Context, path string, r io.ReadSeeker, args ...interface{}) error {
 
 		_, uri_args, err := uri.ParseURI(path)
@@ -55,10 +55,10 @@ func IndexTables(ctx context.Context, opts *IndexTablesOptions, iterator_uri str
 		if opts.Timings {
 			go monitor.Signal(ctx)
 		}
-		
+
 		return nil
 	}
-	
+
 	iter, err := iterator.NewIterator(ctx, iterator_uri, iter_cb)
 
 	if err != nil {
@@ -66,19 +66,19 @@ func IndexTables(ctx context.Context, opts *IndexTablesOptions, iterator_uri str
 	}
 
 	if opts.Timings {
-		
+
 		m, err := timings.NewMonitor(ctx, "counter://PT60S")
-		
+
 		if err != nil {
 			return fmt.Errorf("Failed to create timings monitor, %w", err)
 		}
 
 		monitor = m
-		
+
 		monitor.Start(ctx, os.Stdout)
 		defer monitor.Stop(ctx)
 	}
-	
+
 	err = iter.IterateURIs(ctx, to_iterate...)
 
 	if err != nil {
